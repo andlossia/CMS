c8d5b42f37381dd71e14ed51a1ed34b221f209b7
Andlossia <69584141+andlossia@users.noreply.github.com>
2025-09-02 15:12:38 +0300

    Update create.js

--- controllers/crud/bulk/create.js
+++ controllers/crud/bulk/create.js
@@ -1,21 +1,28 @@
 const { authenticate } = require('../../../middlewares/authentication');
-const {checkUniqueFields} = require('../../../utils/validateUniqueFields');
-const {getSchemaValidator} = require('../../../utils/schemaValidator');
+const { checkUniqueFields } = require('../../../utils/validateUniqueFields');
+const { getSchemaValidator } = require('../../../utils/schemaValidator');
 const { adminDB } = require('../../../database');
 const resolveRelations = require('../../../utils/resolveRelations');
 
- const createManyItems = (Model, modelName, uniqueFields = []) => {
+const createManyItems = (Model, modelName, uniqueFields = []) => {
   const schemaValidator = getSchemaValidator(modelName, adminDB);
 
   return [
     authenticate,
     schemaValidator,
     checkUniqueFields(uniqueFields, Model, modelName),
-    async (req, res) => {
+    async (req, res, next) => {
       const owner = req.user ? req.user.id : null;
+
+      if (!Array.isArray(req.body.items) || req.body.items.length === 0) {
+        return res.badRequest({
+          message: 'Request body must contain an "items" array with at least one element.'
+        });
+      }
 
       try {
         const items = [];
+        const auditEntries = [];
 
         for (const itemData of req.body.items) {
           const linkedObjects = {};
@@ -29,26 +36,34 @@ const resolveRelations = require('../../../utils/resolveRelations');
           const linkedObjectIds = await resolveRelations(linkedObjects, Model);
           const finalItemData = { ...itemData, ...linkedObjectIds, owner };
 
-    
-
           const item = await Model.create(finalItemData);
           items.push(item);
+          auditEntries.push({
+            modelName,
+            documentId: item._id,
+            action: 'create',
+            after: item.toJSON()
+          });
         }
+
+        req.audit = { bulk: auditEntries };
 
         res.created({
           message: `${modelName}s created successfully`,
           data: items.map((item) => item.toJSON()),
         });
+
       } catch (error) {
+        console.error(`🔥 Error creating [${modelName}s]:`, error.message);
         res.internalServerError({
           message: `Error creating ${modelName}s`,
           error: error.message,
         });
       }
+
+      next();
     }
   ];
 };
 
-  
-  module.exports = createManyItems;
-  
+module.exports = createManyItems;
